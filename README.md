be15cfb7eb29324526000bdc829a8b96790168a1
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2022-03-05 17:58:35 +0700

    Starter project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple Rest API with Springboot
+
---
+++ src/main/java/com/restwebservice/RestwebserviceApplication.java
@@ -0,0 +1,14 @@
+package com.restwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestwebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestwebserviceApplication.class, args);
+		System.out.println("Application was running...");
+	}
+
+}
